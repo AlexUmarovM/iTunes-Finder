629344c2797af8d2052d3bb56017e5195937c4d1
Alex Umarov <umarov.iosdev@gmail.com>
2020-09-13 13:02:51 +0300

    MVC Files CollectionView

---
+++ iTunes Finder/Controller/DetailViewController.swift
@@ -0,0 +1,9 @@
+//
+//  DetailViewController.swift
+//  iTunes Finder
+//
+//  Created by Александр Умаров on 13.09.2020.
+//  Copyright © 2020 Александр Умаров. All rights reserved.
+//
+
+import Foundation
---
+++ iTunes Finder/View/AlbumCell.swift
@@ -0,0 +1,9 @@
+//
+//  AlbumCell.swift
+//  iTunes Finder
+//
+//  Created by Александр Умаров on 13.09.2020.
+//  Copyright © 2020 Александр Умаров. All rights reserved.
+//
+
+import Foundation
